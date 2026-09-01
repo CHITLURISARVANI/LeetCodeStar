7088a71c8361efc85c88dc3ba6ee34101667a1b6
CHITLURISARVANI <chitlurisarvani@gmail.com>
2026-09-01 07:42:29 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (24.67%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-            int left = 0;
-            int right = s.length() - 1;
-            while(left < right){
-                if(s[left] == s[right]){
+       int left = 0;
+       int right = s.length() - 1;
+       while(left < right){
+        if(s[left] == s[right]){
+            left++;
+            right--;
+        }
+      
+        else{
+            return palindrome(s,left+1,right) || palindrome(s,left,right-1);
+        }
+       }
+       return true;
+    }
+       bool palindrome(string s,int left,int right){
+        while(left<right){
+                if(s[left] != s[right]){
+                    return false;
+                }
                     left++;
                     right--;
-                }
-                else{
-                    return Palindrome(s,left + 1,right)|| Palindrome(s, left, right - 1);
-                }
-            }
-            return true;
         }
-       
-        bool Palindrome(string s,int left,int right){
-
-        while(left < right ){
-            if(s[left] != s[right]){
-                return false;
-            }
-                 left++;
-                right--;
-            }
-            return true;
-         } 
-        
-    };
+       return true;
+    }
+};
