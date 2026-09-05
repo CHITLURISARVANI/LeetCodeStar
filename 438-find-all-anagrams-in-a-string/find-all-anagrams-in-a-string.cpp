b6187a8669237c37af07ce93d8756afae5c166ac
CHITLURISARVANI <chitlurisarvani@gmail.com>
2026-09-05 21:10:14 +0530

    Time: 1 ms (87.46%) | Memory: 11.6 MB (37.66%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        if (p.size() > s.size()) return ans;
+
+        vector<int> freq(26, 0);
+
+        for (char c : p)
+            freq[c - 'a']++;
+
+        int left = 0, right = 0, count = p.size();
+
+        while (right < s.size()) {
+            if (freq[s[right] - 'a'] > 0)
+                count--;
+
+            freq[s[right] - 'a']--;
+            right++;
+
+            if (right - left > p.size()) {
+                freq[s[left] - 'a']++;
+                if (freq[s[left] - 'a'] > 0)
+                    count++;
+                left++;
+            }
+
+            if (count == 0)
+                ans.push_back(left);
+        }
+
+        return ans;
+    }
+};
