825fce1fa2935b53babc5cd68cd48957440a4de7
CHITLURISARVANI <chitlurisarvani@gmail.com>
2026-09-17 20:01:08 +0530

    Time: 3 ms (91.58%) | Memory: 13.2 MB (61.27%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        // If word2 is empty, delete all characters from word1
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+
+        // If word1 is empty, insert all characters of word2
+        for (int j = 0; j <= n; j++)
+            dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    int insertOp = dp[i][j - 1];
+                    int deleteOp = dp[i - 1][j];
+                    int replaceOp = dp[i - 1][j - 1];
+
+                    dp[i][j] = 1 + min(insertOp,
+                                    min(deleteOp, replaceOp));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
